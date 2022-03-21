0b224a1241d6d38a5513918b50c398ae76cf6034
Levy <levyronald3@gmail.com>
2022-03-21 11:29:35 -0300

    update: Importanto portfolio para App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,24 @@
 <template>
 <div>
   <meu-menu><img src="./assets/logo.svg" alt=""></meu-menu>
+  <portfolio class="portfolio"><img src="./assets/manager.svg" alt=""></portfolio>
 </div>
 </template>
 
 <script>
 import menu from './components/shared/menu/menu.vue'
+import portfolio from './components/portfolio/portfolio.vue'
 export default {
       components: {
-    'meu-menu' : menu
+    'meu-menu' : menu,
+    'portfolio': portfolio
     
   },
 }
 </script>
 
 <style>
+.portfolio{
+  display: none;
+}
 </style>
