94301210113b4dd278e96b8ab05d2c66e2e6f8d0
lucas <lucas.santo@tallos.com.br>
2022-03-21 09:54:53 -0300

    update: adicionando components menu

---
+++ src/components/shared/menu/menu.vue
@@ -0,0 +1,56 @@
+<template>
+ <header class="menu">
+     <slot>
+     </slot>
+    <nav>
+        <li> <a href=""> Home </a></li>
+        <li> <a href=""> Portfólio </a></li>
+        <li> <a href=""> Contato </a></li> 
+    </nav>      
+    </header>
+</template>
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+.menu{
+	display: flex;
+	align-items: center;
+	background-color: #ffffff;
+  padding: 30px;
+  width: 1111px;
+  height: 32px;
+  left: 165px;
+  top: 64px;
+}
+
+.menu nav{
+	list-style: none;	
+	display: flex;
+  justify-content: flex-end;
+	width: 100%;
+}
+
+.menu nav li{
+	text-align: center;
+	width: 10%;
+}
+
+.menu nav li a{
+  font-family: 'Public Sans';
+	display: inline-block;
+	padding: 10px 0;
+	width: 100%;
+	text-decoration: none;
+	color: #33323D;
+  font-size: 20px;
+}
+
+.menu nav li a:hover{
+	transition: 0.5s;
+  color: #5FB4A2;
+}
+</style>
