8aba16fa3965fe963dbf1151b0ff45fd2cd42f18
lucas <lucas.santos121252@gmail.com>
2022-03-22 11:00:32 -0300

    update: iniciando projeto manager e adicionando interessados como components

---
+++ src/components/portfolio/portfolio-pages/manager/manager.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <interessados></interessados>
+  </div>
+</template>
+<script>
+import interessados from '../../../shared/interessados/interessados.vue'
+
+export default {
+      components: {
+        'interessados': interessados
+  },
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/shared/interessados/interessados.vue
@@ -0,0 +1,29 @@
+<template>
+    <section class="interessado">
+      <h1>Interessado em fazer projetos comigo?</h1>
+      <a href="/contato" class="ctn">contato</a>
+    </section>
+</template>
+<style>
+.interessado{
+  display:flex;
+  padding:10% 10%;
+  align-items:center;
+  justify-content: space-between;
+}
+
+.interessado h1{
+  font-family: 'Ibarra Real Nova','Bold';
+  font-size:2.2em;
+  max-width:400px;
+}
+
+.interessado a{
+  font-family: 'Public Sans',sans-serif;
+  text-decoration:none;
+  text-transform: uppercase;
+  padding:15px 40px;
+  border: 1px solid #33323D;
+  color:#33323D;
+}
+</style>
